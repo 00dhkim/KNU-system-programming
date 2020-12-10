f02d462f1a85e961c0d01896f503a2a4552a76de
Dohyun Kim <00dh.kim@gmail.com>
2020-12-10 18:35:19 +0900

    readme.md

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <curses.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
 
 extern int di;
 extern int dj;
@@ -12,10 +16,10 @@ extern void what_is_direction(int direc);
 extern void move_();
 extern void print_map();
 
-int main()
-{
+// 게임 실행을 담당
+void child_code() {	
+	
 	print_home();
-
 	init_();
 
 	while (1) {
@@ -32,6 +36,29 @@ int main()
 		move_();
 	}
 
+	exit(0);
+}
+
+void parent_code() {
+	
+	wait(0);
+	puts("THIS IS THE END");
+}
+
+int main() {
+
+	int pid = fork();
+
+	switch(pid) {
+		case -1:
+			perror("fork");
+			exit(1);
+		case 0: // child
+			child_code();
+			break;
+		default: // parent
+			parent_code();
+	}
 
 	return 0;
 }
